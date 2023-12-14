d6c57a0083d81cb02d1d9922b3f74f12d4c3a4e1
Nistor Ioan Gabriel <126780933+nistor1@users.noreply.github.com>
2023-12-14 18:58:56 +0200

    1.3

--- src/main/java/org/launchcode/hellospring/controllers/EventController.java
+++ src/main/java/org/launchcode/hellospring/controllers/EventController.java
@@ -1,4 +1,28 @@
 package org.launchcode.hellospring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+@Controller
+@RequestMapping("events")
 public class EventController {
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Code");
+        events.add("Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
 }
